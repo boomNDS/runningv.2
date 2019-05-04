5b321573110a9721fc649eaf4b9d558ade0dd839
boomNDS <boomsixboom@gmail.com>
2019-05-04 15:48:01 +0700

    add backend

--- runningv2/urls.py
+++ runningv2/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('', include('frontend.urls')),
+    path('backend', include('backend.urls')),
     path('admin/', admin.site.urls),
 ]
